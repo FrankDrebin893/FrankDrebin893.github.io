67852d4c126a4998f64ef9d860d8c474583f9957
Rasmus Højte <Rasmus Hjte>
2017-01-22 19:46:31 +0100

    background image fix

--- app.js
+++ app.js
@@ -8,17 +8,17 @@ const webpackConfig = require('./webpack.config.js');
 
 const compiler = webpack(webpackConfig);
 
-//app.use('/public', express.static(path.join(__dirname, 'src/client/public')));
-//app.use(express.static(__dirname + '/src/client/public'));
+app.use('/public', express.static(path.join(__dirname, 'src/client/public')));
+app.use(express.static(__dirname + '/src/client/public'));
 
-
+/*
 app.get('*', function (req, res) {
   res.sendFile(__dirname + '/src/client/public/indexTwo.html');
-})
-
-/*app.get('*', function (request, response){
-  response.sendFile(path.resolve(__dirname, 'src/client/public', 'indexTwo.html'))
 })*/
+
+app.get('*', function (request, response){
+  response.sendFile(path.resolve(__dirname, 'src/client/public', 'indexTwo.html'))
+})
 
 var server = app.listen(port, function () {
   console.log('Example app listening on port ' + port + '!');
