7341ad7d54cef8421e529208b8e0019e1134fe8f
Rasmus Højte <rasmusvh@msn.com>
2023-04-05 13:01:37 +0200

    Update bio text

--- src/components/bio.js
+++ src/components/bio.js
@@ -24,13 +24,16 @@ const Bio = () => {
         Hi, I'm Rasmus. 
 		
 		This portfolio is mainly a recreational outlet that I return to once in a while.
-		
-        You can find me at{" "}
+		<br />
+        You can find me on 
         <a href="https://www.linkedin.com/in/rasmus-h%C3%B8jte-4a2326110/">
           LinkedIn
-        </a>
-        and <a href="https://github.com/FrankDrebin893">Github</a>. I also post terrible sound bites to{" "}
-        <a href="https://soundcloud.com/rasmus-valbro-h-jte">Soundcloud</a> and{" "}
+        </a>		
+        and <a href="https://github.com/FrankDrebin893">Github</a>. 
+		
+		<br />
+		I also post terrible sound bites to 
+        <a href="https://soundcloud.com/rasmus-valbro-h-jte">Soundcloud</a> and 
         <a href="https://www.youtube.com/@Cruize91/videos">YouTube</a>
       </p>
     </div>
